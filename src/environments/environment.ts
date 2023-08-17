4353f4d2286a421a5e697ecf7063ca27f49f487c
mcimino-woolies <mcimino2@woolworths.com.au>
2023-08-17 11:00:57 +1000

    Env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,5 @@ import {Environment} from "./environment.interface";
 export const environment: Environment = {
   production: false,
   title: '',
-  apiUrl: '',
-  googleAnalyticsId: '',
-  featureToggle: '',
-  specialMessage: ''
+  iframeUrl: ' '
 }
